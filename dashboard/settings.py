47648a138cbc7e617095872e174d28b3da6a2960
Ulug'bek Istamov <89534537+ulugbekistamov@users.noreply.github.com>
2022-01-07 16:56:27 +0500

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--y+csq5u55uybsy^kcwdi@54l9_i0^^6rj2(fleb3s@&8ox-1&'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
